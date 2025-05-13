6d06476727b7d831cc75ed684659276a076cbe6f
Shiro&Tiger <tiger1538@icloud.com>
2025-05-13 21:43:38 +0900

    update

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xcodebuild archive -archivePath archive -scheme LiveContainer -project LiveContainer.xcodeproj -sdk iphoneos -arch arm64 -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
